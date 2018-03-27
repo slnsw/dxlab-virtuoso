348c6636ed470617757a780c9c879383d8c28361
Kaho Cheung <kaho@elefanttraks.com>
2018-03-28 09:52:17 +1100

    Added popover

---
+++ components/Popover/Popover.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+import './Popover.css';
+
+class Popover extends Component {
+  // static propTypes = {};
+
+  render() {
+    // const {} = this.props;
+
+    return <div className="popover" />;
+  }
+}
+
+export default Popover;
