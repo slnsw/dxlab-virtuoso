dc0bfc85a5ec068b1d4ccf83477bba326da3f761
Kaho Cheung <kaho@elefanttraks.com>
2020-03-13 09:40:01 +1100

    Update now.staging.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
-    require("postcss-easy-import")({
-      prefix: "_",
+    require('postcss-easy-import')({
+      prefix: '_',
     }), // keep this first
-    require("postcss-mixins"),
-    require("postcss-cssnext")({
+    require('postcss-mixins'),
+    require('postcss-cssnext')({
       features: {
         autoprefixer: false, // Next.js already runs autoprefixer
       },
@@ -14,4 +14,27 @@ module.exports = {
     // require('cssnano')()
     require('postcss-reporter'),
   ],
-}
+};
+
+// Use this config after removing next-css
+// module.exports = {
+//   plugins: [
+//     [
+//       'postcss-easy-import',
+//       {
+//         prefix: '_',
+//       },
+//     ],
+//     'postcss-mixins',
+//     [
+//       'postcss-cssnext',
+//       {
+//         features: {
+//           autoprefixer: false, // Next.js already runs autoprefixer
+//         },
+//       },
+//     ],
+//     'lost',
+//     'postcss-reporter',
+//   ],
+// };
