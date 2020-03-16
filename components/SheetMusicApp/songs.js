db0ee8962bfa02a8cd0bb281ae2b35fef8d211d4
Luke Dearnley <luked@phm.gov.au>
2020-03-16 15:34:19 +1100

    tidying up notation string

--- components/SheetMusicApp/songs.js
+++ components/SheetMusicApp/songs.js
@@ -2,7 +2,7 @@ const songs = [
   {
     title: 'Our Sailor Prince by J.C. Neild Jr, 1867',
     url: 'https://collection.sl.nsw.gov.au/digital/file/06ddDNk67LV8G',
-    notation: `X:1\nM:4/4\nK:A\nL:1/4\n|:EA/c/BG/F/|Eec2|AG/B/EF|G2z2:|\n`,
+    notation: `X:1\nM:4/4\nK:A\nL:1/4\n|:c2 z2|E A/ c/ B G/F/|E e c2|\nA G/ B/ E F|G2 z2|E A/ c/ B G/F/:|\n`,
     bpm: 70,
   },
 ];
