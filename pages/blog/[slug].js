7c194b7b24e6d1ffa04d929bd74ad286aa88f1d6
Kaho Cheung <kaho@elefanttraks.com>
2020-03-23 17:27:28 +1100

    Revert SSG for blog posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { withRouter } from 'next';
-import { ApolloProvider } from '@apollo/react-hooks';
-// import { graphql } from 'react-apollo';
+// import { ApolloProvider } from '@apollo/react-hooks';
+import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import WebsiteApp from '../../components/WebsiteApp';
@@ -11,13 +10,16 @@ import Button from '../../components/Button';
 import Comments from '../../components/Comments';
 import Four04 from '../_error';
 
-import { initApolloClient } from '../../lib/apollo';
+import {
+  withApollo,
+  // initApolloClient
+} from '../../lib/apollo';
 import { formatDate } from '../../lib';
 
 import '../post.css';
 import '../../styles/gallery.css';
 
-const client = initApolloClient();
+// const client = initApolloClient();
 
 class Post extends Component {
   static propTypes = {
@@ -91,15 +93,11 @@ class Post extends Component {
       author,
       router,
       date,
-      // loading,
+      loading,
       comments,
       experiments,
       posts,
     } = this.props;
-
-    // console.log(router);
-
-    const loading = router.isFallback;
 
     const featuredImageUrl = featuredMedia && featuredMedia.sourceUrl;
     const featuredImageDescription = featuredMedia && featuredMedia.description;
@@ -113,66 +111,66 @@ class Post extends Component {
     }
 
     return (
-      <ApolloProvider client={client}>
-        <WebsiteApp
-          isLoading={loading}
-          pathname={`/blog/${router.query.slug}`}
-          title={title}
-          metaDescription={excerpt}
-          metaImageUrl={featuredImageUrl}
-          metaImageAlt={featuredImageDescription}
-        >
-          <article className="post container container--md">
-            <div>
-              <div className="post__featured-image-holder">
-                <img
-                  className="post__featured-image"
-                  src={featuredImageUrl}
-                  alt={featuredImageDescription}
-                />
-                <div className="post__date">{dateString}</div>
+      // <ApolloProvider client={client}>
+      <WebsiteApp
+        isLoading={loading}
+        pathname={`/blog/${router.query.slug}`}
+        title={title}
+        metaDescription={excerpt}
+        metaImageUrl={featuredImageUrl}
+        metaImageAlt={featuredImageDescription}
+      >
+        <article className="post container container--md">
+          <div>
+            <div className="post__featured-image-holder">
+              <img
+                className="post__featured-image"
+                src={featuredImageUrl}
+                alt={featuredImageDescription}
+              />
+              <div className="post__date">{dateString}</div>
+            </div>
+
+            <header className="post__header">
+              <h1 className="post__title">{title}</h1>
+              <div className="post__author">
+                By <a href={`/search?q=${authorName}`}>{authorName}</a>
               </div>
 
-              <header className="post__header">
-                <h1 className="post__title">{title}</h1>
-                <div className="post__author">
-                  By <a href={`/search?q=${authorName}`}>{authorName}</a>
-                </div>
+              <div className="post__cta">
+                {experimentUrl && (
+                  <Button href={experimentUrl} target="_blank">
+                    LAUNCH EXPERIMENT
+                  </Button>
+                )}
+                {githubUrl && (
+                  <Button href={githubUrl} target="_blank">
+                    CODE
+                  </Button>
+                )}
+              </div>
+            </header>
 
-                <div className="post__cta">
-                  {experimentUrl && (
-                    <Button href={experimentUrl} target="_blank">
-                      LAUNCH EXPERIMENT
-                    </Button>
-                  )}
-                  {githubUrl && (
-                    <Button href={githubUrl} target="_blank">
-                      CODE
-                    </Button>
-                  )}
-                </div>
-              </header>
+            <div
+              className="post__content"
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
 
-              <div
-                className="post__content"
-                dangerouslySetInnerHTML={{ __html: content }}
-              />
+            <ShareBox
+              title={title}
+              text={excerpt}
+              pathname={`/blog/${router.query.slug}`}
+            />
 
-              <ShareBox
-                title={title}
-                text={excerpt}
-                pathname={`/blog/${router.query.slug}`}
-              />
+            <br />
+            <br />
+            <br />
 
-              <br />
-              <br />
-              <br />
-
-              {id && <Comments postId={id} comments={comments} />}
-            </div>
-          </article>
-        </WebsiteApp>
-      </ApolloProvider>
+            {id && <Comments postId={id} comments={comments} />}
+          </div>
+        </article>
+      </WebsiteApp>
+      // </ApolloProvider>
     );
   }
 }
@@ -207,50 +205,74 @@ const postQuery = gql`
   }
 `;
 
-const postsQuery = gql`
-  query Posts {
-    posts(limit: 5) {
-      slug
-    }
-  }
-`;
-
-export const getStaticPaths = async () => {
-  const client2 = initApolloClient();
-
-  const { data } = await client2.query({
-    query: postsQuery,
-  });
-
-  const paths = data.posts.map((post) => {
-    return {
-      params: {
-        slug: post.slug,
+// The `graphql` wrapper executes a GraphQL query and makes the results
+// available on the `data` prop of the wrapped component (ExamplePage)
+export default withApollo(
+  graphql(postQuery, {
+    options: ({
+      router: {
+        query: { slug },
       },
-    };
-  });
-
-  return {
-    paths,
-    fallback: true,
-  };
-};
-
-export const getStaticProps = async ({ params }) => {
-  const client2 = initApolloClient();
-
-  const { data } = await client2.query({
-    query: postQuery,
-    variables: {
-      slug: params.slug,
+    }) => {
+      return {
+        variables: {
+          slug,
+        },
+      };
     },
-  });
+    props: ({ data }) => {
+      const post = data.posts && data.posts[0];
 
-  const post = data.posts && data.posts[0];
+      return {
+        ...data,
+        ...post,
+      };
+    },
+  })(Post),
+);
 
-  return {
-    props: { ...data, ...post },
-  };
-};
+// Attempt to get dynamic static site working
+// When deployed, Zeit is unable to build fallback posts
+// const postsQuery = gql`
+//   query Posts {
+//     posts(limit: 5) {
+//       slug
+//     }
+//   }
+// `;
 
-export default Post;
+// export const getStaticPaths = async () => {
+//   const { data } = await client.query({
+//     query: postsQuery,
+//   });
+
+//   const paths = data.posts.map((post) => {
+//     return {
+//       params: {
+//         slug: post.slug,
+//       },
+//     };
+//   });
+
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// };
+
+// export const getStaticProps = async ({ params }) => {
+//   const { data } = await client.query({
+//     query: postQuery,
+//     variables: {
+//       slug: params.slug,
+//     },
+//   });
+
+//   const post = data.posts && data.posts[0];
+
+//   return {
+//     props: { ...data, ...post },
+//   };
+// };
+
+// export default Post;
