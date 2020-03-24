61233a1f3454a7658990b4a7e854f47dfa077f3d
Luke Dearnley <luked@phm.gov.au>
2020-03-24 18:12:37 +1100

    changed VOICE to use clarinet sound

--- components/SheetMusicApp/SheetMusicApp.js
+++ components/SheetMusicApp/SheetMusicApp.js
@@ -10,19 +10,43 @@ import './SheetMusicApp.css';
 
 const AUDIO_BASE_URL =
   // 'https://dxlab-website.s3-ap-southeast-2.amazonaws.com/audio/vsco-2-ce/Keys';
-  'https://dxlab.sl.nsw.gov.au/files/audio/vsco-2-ce/Keys';
+  'https://dxlab.sl.nsw.gov.au/files/audio/vsco-2-ce';
 
 const pianoSamples = {
-  A1: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_012.mp3`,
-  'C#2': `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_014.mp3`,
-  F2: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_016.mp3`,
-  A2: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_018.mp3`,
-  'C#3': `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_020.mp3`,
-  F3: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_022.mp3`,
-  A3: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_024.mp3`,
-  'C#4': `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_026.mp3`,
-  F4: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_028.mp3`,
-  G4: `${AUDIO_BASE_URL}/Upright+Piano/Player_dyn3_rr1_030.mp3`,
+  A1: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_012.mp3`,
+  'C#2': `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_014.mp3`,
+  F2: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_016.mp3`,
+  A2: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_018.mp3`,
+  'C#3': `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_020.mp3`,
+  F3: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_022.mp3`,
+  A3: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_024.mp3`,
+  'C#4': `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_026.mp3`,
+  F4: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_028.mp3`,
+  G4: `${AUDIO_BASE_URL}/Keys/Upright+Piano/Player_dyn3_rr1_030.mp3`,
+};
+
+// const fluteSamples = {
+//   A3: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_A3_v1_1.mp3`,
+//   C3: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_C3_v1_1.mp3`,
+//   E3: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_E3_v1_1.mp3`,
+//   A4: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_A4_v1_1.mp3`,
+//   C4: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_C4_v1_1.mp3`,
+//   E4: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_E4_v1_1.mp3`,
+//   A5: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_A5_v1_1.mp3`,
+//   C5: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_C5_v1_1.mp3`,
+//   E5: `${AUDIO_BASE_URL}/Woodwinds/Flute/expvib/LDFlute_expvib_E5_v1_1.mp3`,
+// };
+
+const clariSamples = {
+  D2:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_D2_v3_rr1_sum.mp3`,
+  F2:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_F2_v3_rr1_sum.mp3`,
+  'A#2': `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_A%232_v3_rr1_sum.mp3`,
+  D3:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_D3_v3_rr1_sum.mp3`,
+  F3:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_F3_v3_rr1_sum.mp3`,
+  'A#3': `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_A%233_v3_rr1_sum.mp3`,
+  D4:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_D4_v3_rr1_sum.mp3`,
+  F4:    `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_F4_v3_rr1_sum.mp3`,
+  'A#4': `${AUDIO_BASE_URL}/Woodwinds/Clarinet/susLong/DCClar_susLong_A%234_v3_rr1_sum.mp3`,
 };
 
 const SheetMusicApp = ({ className }) => {
@@ -125,7 +149,7 @@ const SheetMusicApp = ({ className }) => {
           <Instrument
             type="sampler"
             notes={vocalNotes}
-            samples={pianoSamples}
+            samples={clariSamples}
             options={{
               release: '2n',
             }}
