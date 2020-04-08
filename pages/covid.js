ec70c3051ffed969d7ff656bd2b80f3ef84f5bf8
Luke Dearnley <luked@phm.gov.au>
2020-04-08 15:38:46 +1000

    starting to get covid posts going

---
+++ pages/covid.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import WPAPI from 'wpapi';
+
+function yyyymmdd() {
+  const date = new Date();
+  const mm = `${date.getMonth() + 1 > 9 ? '' : '0'}${date.getMonth() + 1}`; // getMonth() is zero-based
+  const dd = `${date.getDate() > 9 ? '' : '0'}${date.getDate()}`;
+  const hh = `${date.getHours() > 9 ? '' : '0'}${date.getHours()}`;
+  const mins = `${date.getMinutes() > 9 ? '' : '0'}${date.getMinutes()}`;
+  const ss = `${date.getSeconds() > 9 ? '' : '0'}${date.getSeconds()}`;
+
+  return `${date.getFullYear()}-${mm}-${dd}-${hh}-${mins}-${ss}`;
+}
+
+const postName = yyyymmdd();
+console.log(postName);
+console.log(process.env.WP_API_ENDPOINT);
+
+const wp = new WPAPI({
+  endpoint: process.env.WP_API_ENDPOINT,
+  username: process.env.WP_USERNAME,
+  password: process.env.WP_PASSWORD,
+});
+
+// wp.storyPost = wp.registerRoute(
+//     'acf/v3',
+//     '/posts/(?P<id>\\d+)',
+// );
+
+// const postType = wp.instagramSelfies();
+async function test() {
+  try {
+    // wp.posts().then(function( data ) {
+    //     // do something with the returned posts
+    // }).catch(function( err ) {
+    //     // handle error
+    // });
+
+    const newPost = await wp.posts().create({
+      title: postName,
+      slug: postName,
+      content: 'Not quite 300 words but I am only testing.',
+      status: 'publish',
+      meta: {
+        name: 'test name',
+        email: 'test@email.com',
+      },
+    });
+    console.log(newPost);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const CovidPage = () => {
+//   test();
+  return (
+    <div>
+      <h1 onClick={()=>{test()}}>Covid</h1>
+    </div>
+  );
+};
+
+export default CovidPage;
