918abd21c3f2a3c02a7e78f2a1db68e592d0fbf9
Luke Dearnley <luked@phm.gov.au>
2020-05-02 18:35:28 +1000

    updated TextTruncate.js so it no longer chops text strings in the middle of HTML tags

--- components/TextTruncate/TextTruncate.js
+++ components/TextTruncate/TextTruncate.js
@@ -103,23 +103,20 @@ const TextTruncate = ({
     } else {
       childText = child.props.children;
     }
-    // console.log('--------');
-    // console.log(childText);
     return childText;
   }).join(' ');
 
-  // console.log('=====');
-  // console.log(text.length);
-  const isTextOverLimit = text.length > limit;
+  // Don't really want to count the HTML tags in the length
+  const isTextOverLimit = stripHtml(text).length > limit;
 
   let result;
 
   if (isTextOverLimit && isActive) {
     const strippedText = shouldStripHtml ? stripHtml(text) : text;
-    const truncatedText = `${strippedText
-      .slice(0, limit)
-      .trimRight()}${ellipsisChars}`;
-
+    // const truncatedText = `${strippedText
+    //   .slice(0, limit)
+    //   .trimRight()}${ellipsisChars}`;
+    const truncatedText = truncateNicely(strippedText, limit, ellipsisChars);
     // Assign processed text depending on firstChildrenType
     if (firstChildrenType === 'string') {
       result = truncatedText;
@@ -187,6 +184,120 @@ const TextTruncate = ({
 
 function stripHtml(text) {
   return text.replace(/(<([^>]+)>)/gi, '');
+}
+
+function truncateNicely(text, limit, endChar) {
+  // Writen by LukeD Apr/May 2020
+  // NOTE: uses generateClosingTag() function below.
+  // Function is passed a text string to be truncated to 'limit' chars long
+  // But it may contain nested HTML <tags> which we don't want to truncate in
+  // the middle of leaving this: <tag... (we also don't really want to count
+  // them as they don't generally contribute to the length of text rendered.)
+  // Also we want to ensure the correct closing tags exit after the truncation
+  // so the returned text is valid HTML.
+
+  const splitByTags = text.split(/(<[^>]+>)/gi);
+  // The above returns an array something like:
+  // [ 'text', 'tag', 'text', 'tag', 'text', 'tag', 'text', 'tag', 'text' ]
+
+  const stack = []; // place to remember opened tags that might need to closed after truncation
+  const tagList = [];
+  const textList = [];
+
+  // Split it into two arrays, one of tags, one of texts.
+  // But keep positions intact, so we get:
+  // [ null, 'tag', null, 'tag', null, 'tag', null, 'tag', null ]
+  // [ 'text', null, 'text', null, 'text', null, 'text', null, 'text' ]
+
+  for (let i = 0; i < splitByTags.length; i++) {
+    if (splitByTags[i].slice(0, 1) === '<') {
+      tagList[i] = splitByTags[i];
+    } else {
+      textList[i] = splitByTags[i];
+    }
+  }
+  console.log(textList); // ///////////////////////////////// remove later
+  console.log(tagList); // ///////////////////////////////// remove later
+
+  // Now start reassembling, counting length of text parts only
+  let lengthSoFar = 0;
+  let out = '';
+  let overLimit = false;
+  for (let i = 0; i < splitByTags.length; i++) {
+    console.log(lengthSoFar, i, stack); // ///////////////////////////////// remove later
+    if (!overLimit) {
+      // We are still under the character limit.
+      // First deal with next bit of text
+      if (textList[i] && textList[i] !== '\n') {
+        const textBitLength = textList[i].length;
+        if (lengthSoFar + textBitLength > limit) {
+          const truncBitLength = limit - lengthSoFar;
+          const truncText = textList[i].slice(0, truncBitLength);
+          out += `${truncText}${endChar}`;
+          lengthSoFar += truncBitLength;
+          overLimit = true;
+          console.log('OVER!!'); // ///////////////////////////////// remove later
+        } else {
+          out += textList[i];
+          lengthSoFar += textBitLength;
+        }
+      }
+      // Next add back in the next tag
+      if (tagList[i]) {
+        out += tagList[i];
+        // push them onto the stack, unless they self-closing
+        if (splitByTags[i].slice(-2) !== '/>') {
+          // or are the closing tag for the top of the stack
+          if (
+            stack.length > 0 &&
+            generateClosingTag(stack[stack.length - 1]) === splitByTags[i]
+          ) {
+            stack.pop();
+            // its not the closing tag for the top of the stack, so
+            // if its not a 'rogue' closing tag, it will be an opening
+            // tag, so add it to the stack.
+          } else if (splitByTags[i].slice(0, 2) !== '</') {
+            stack.push(splitByTags[i]);
+          }
+        }
+      }
+    }
+  }
+  // now add closing tags for any tags left in the stack
+  while (overLimit && stack.length > 0) {
+    const closing = generateClosingTag(stack.pop());
+    if (closing) {
+      out += closing;
+    }
+  }
+  console.log('in: ', text); // ///////////////////////////////// remove later
+  console.log('out: ', out); // ///////////////////////////////// remove later
+  return out;
+}
+
+function generateClosingTag(tag) {
+  // Writen by LukeD Apr/May 2020
+  // This function generates the closing
+  // tag that corresponds to any opening tag.
+  // EG </p> for a <p>, </div> for <div class="somthing">
+  // or </ul> for <ul style="color: black" class="someclass">
+  if (!tag) {
+    // nothing to do...
+    return null;
+  }
+  if (tag.slice(0, 2) === '</') {
+    // it is already a closing tag!
+    return tag;
+  }
+  if (tag.slice(-2) === '/>') {
+    // its is a self-closing tag!
+    return tag;
+  }
+  // match from after the < up until the first whitespace or the >
+  const inners = tag.match(/<([^\s>]+).*/i);
+  if (inners[1]) {
+    return `</${inners[1]}>`;
+  }
 }
 
 TextTruncate.propTypes = {
