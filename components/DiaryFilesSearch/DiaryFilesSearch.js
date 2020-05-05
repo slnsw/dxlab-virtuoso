d69fc292c25e7042f475df7868ea826d66cab941
Luke Dearnley <luked@phm.gov.au>
2020-05-05 12:51:34 +1000

    adding search box to search page WIP

--- components/DiaryFilesSearch/DiaryFilesSearch.js
+++ components/DiaryFilesSearch/DiaryFilesSearch.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 
 import LoaderText from '../LoaderText';
 import DiaryFilesPost from '../DiaryFilesPost';
@@ -18,16 +19,47 @@ const DiaryFilesSearch = ({ className, search }) => {
   const posts = data && data.diaryFiles && data.diaryFiles.posts;
   // const { title, authorName, relatedPosts } = posts[0] || {};
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { q } = e.target.elements;
+    console.log(q);
+    Router.push(`/diary-files/search?q=${q.value}`);
+  };
+
   if (loading) {
     return <LoaderText />;
   }
 
-  if (error || !search || posts.length === 0) {
+  if (search && (error || posts.length === 0)) {
     // title === 'Hello World' ||
     return (
       <div className={[css.diaryFilesSearch, className || ''].join(' ')}>
         <h1>No stories found</h1>
       </div>
+    );
+  }
+
+  if (!search) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div
+          className={`${css['diaryFilesForm__section']} ${css['diaryFilesForm__date']}`}
+        >
+          <label htmlFor="q">Search</label>
+          <input
+            name="q"
+            aria-label="q"
+            type="text"
+            aria-required="true"
+            placeholder="Type something..."
+          />
+        </div>
+        <div className={css['diaryFilesForm__submitWrapper']}>
+          <button className="button" type="submit" aria-label="Submit Button.">
+            Search
+          </button>
+        </div>
+      </form>
     );
   }
 
