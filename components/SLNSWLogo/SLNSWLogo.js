ae3f88eb5e4457fc70e844110df70a09dbf8509a
Kaho Cheung <kaho@elefanttraks.com>
2020-05-05 20:16:03 +1000

    Add handlers to DF mobile menu

--- components/SLNSWLogo/SLNSWLogo.js
+++ components/SLNSWLogo/SLNSWLogo.js
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 
 import css from './SLNSWLogo.module.scss';
 
-const SLNSWLogo = ({ className }) => {
+const SLNSWLogo = ({ className, onClick }) => {
   return (
     <div className={[css.slnswLogo, className || ''].join(' ')}>
-      <a href="http://sl.nsw.gov.au" target="_blank" rel="noopener noreferrer">
+      <a
+        href="http://sl.nsw.gov.au"
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={onClick}
+      >
         <img src="/images/logo-slnsw-white.png" alt="State Library Logo." />
       </a>
     </div>
