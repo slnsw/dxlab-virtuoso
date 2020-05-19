015082bf90b1818ea6154dc7589fa2ce3a3fe52c
Kaho Cheung <kaho@elefanttraks.com>
2020-05-19 21:41:44 +1000

    Update changelog. Remove server.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,7 @@ module.exports = {
   rules: {
     // Next JS has a Link component that wraps around <a></a>, inserting the href during compilation. Disable for now.
     'jsx-a11y/anchor-is-valid': 0,
-    // 'comma-dangle': ['warn', 'always-multiline'],
     'react/react-in-jsx-scope': 0,
-    // 'react/jsx-max-props-per-line': [1, { maximum: 3 }],
-    // 'no-tabs': 0,
     'padded-blocks': 0,
     'react-hooks/rules-of-hooks': 'error',
     // Enable Typescript imports to be recognised
