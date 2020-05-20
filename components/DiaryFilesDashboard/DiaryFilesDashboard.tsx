9c2d9abcd851675adfa2b456764422904865cb92
Luke Dearnley <luked@phm.gov.au>
2020-05-20 11:46:24 +1000

    more tidying of Dashboard

--- components/DiaryFilesDashboard/DiaryFilesDashboard.tsx
+++ components/DiaryFilesDashboard/DiaryFilesDashboard.tsx
@@ -6,9 +6,9 @@ import Router from 'next/router';
 
 // import Link from '../Link';
 import LoaderText from '../LoaderText';
-// import HenryLawsonPen from '../DiaryFilesHome/HenryLawsonPen';
+import HenryLawsonPen from '../DiaryFilesHome/HenryLawsonPen';
 import BubbleChart from '../BubbleChart';
-import CTAButtonV2 from '../CTAButtonV2';
+// import CTAButtonV2 from '../CTAButtonV2';
 
 import css from './DiaryFilesDashboard.module.scss';
 
@@ -32,11 +32,8 @@ const DiaryFilesDashboard = ({ className }) => {
   }, []);
 
   const wordsData = data && data.wordsAndCounts ? data.wordsAndCounts : [];
-  const agesData =
-    data && data.agesNumeric
-      ? data.agesNumeric.sort((a, b) => b.item - a.item)
-      : [];
-  const agesData2 = data && data.agesGrouped ? data.agesGrouped : [];
+
+  const agesData = data && data.agesGrouped ? data.agesGrouped : [];
 
   const statesData = data?.states?.map((p) => {
     return { item: p.item === '' ? 'Not supplied' : p.item, count: p.count };
@@ -45,8 +42,13 @@ const DiaryFilesDashboard = ({ className }) => {
     <article className={[css.diaryFilesDashboard, className || ''].join(' ')}>
       <h1>Dashboard</h1>
 
-      {/* <HenryLawsonPen className={css.henryLawsonPen}></HenryLawsonPen> */}
-
+      <a
+        href="https://collection.sl.nsw.gov.au/record/YezdP8A9#26x6POGQkAdo0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <HenryLawsonPen className={css.henryLawsonPen}></HenryLawsonPen>
+      </a>
       {loading && (
         <LoaderText
           style={{
@@ -115,110 +117,24 @@ const DiaryFilesDashboard = ({ className }) => {
           //   Router.push(`/diary-files/search?q=${d.data.name}`);
           // }}
         />
-
-        {/* <BarChart data={wordsData.slice(0, 10)} direction={'vertical'} /> */}
-
-        {/* <p>
-          <CTAButtonV2
-            disabled={popularWordsOffset - 16 <= 0}
-            onClick={() => setPopularWordsOffset(popularWordsOffset - 16)}
-          >
-            Prev
-          </CTAButtonV2>
-          &#160; &#160;
-          <CTAButtonV2
-            disabled={popularWordsOffset + 16 >= wordsData.length}
-            onClick={() => setPopularWordsOffset(popularWordsOffset + 16)}
-          >
-            Next
-          </CTAButtonV2>
-        </p> */}
       </section>
 
-      {/* <BarChart
-        data={agesData}
-        direction={'horizontal'}
-        height={agesData.length * 12}
-        margin={{ top: 10, left: 100, right: 10, bottom: 20 }}
-      /> */}
       <section>
         <h2>Ages of contributors</h2>
         <p>Number of entries by age group</p>
         <BarChart
-          data={agesData2}
+          data={agesData}
           direction={'vertical'}
           showValues={true}
-          height={agesData2.length * 20}
+          height={agesData.length * 20}
           margin={{ top: 10, left: 40, right: 10, bottom: 20 }}
         />
       </section>
-      {/* <div>
-        <ul>
-          {data?.ages
-            ?.sort((a, b) => b.count - a.count)
-            .slice(0, 25)
-            ?.map((p) => {
-              return (
-                <li key={p.item}>
-                  {p.item === '' ? 'Not supplied' : p.item}{' '}
-                  <span>({p.count})</span>
-                </li>
-              );
-            })}
-        </ul>
-      </div> */}
-
-      {/* <section>
-        <h2>Cities</h2>
-        <ul>
-          {data?.cities
-            ?.sort((a, b) => b.count - a.count)
-            .slice(0, 25)
-            ?.map((p) => {
-              return (
-                <li key={p.item}>
-                  {p.item === '' ? 'Not supplied' : p.item}{' '}
-                  <span>({p.count})</span>
-                </li>
-              );
-            })}
-        </ul>
-      </section> */}
-
-      {/* <section>
-        <h2>Postcodes</h2>
-        <ul>
-          {data?.postcodes
-            ?.sort((a, b) => b.count - a.count)
-            .slice(0, 25)
-            ?.map((p) => {
-              return (
-                <li key={p.item}>
-                  {p.item === '0' ? 'Not supplied' : p.item}{' '}
-                  <span>({p.count})</span>
-                </li>
-              );
-            })}
-        </ul>
-      </section> */}
 
       <section>
         <h2>States</h2>
         <p>Number of entries by state</p>
         <BarChart data={statesData} direction={'vertical'} showValues={true} />
-        {/* <ul>
-          {data?.states
-            ?.sort((a, b) => b.count - a.count)
-            .slice(0, 25)
-            ?.map((p) => {
-              return (
-                <li key={p.item}>
-                  {p.item === '' ? 'Not supplied' : p.item}{' '}
-                  <span>({p.count})</span>
-                </li>
-              );
-            })}
-        </ul> */}
 
         <p>
           {data?.overseasEntriesCount} entr
@@ -228,7 +144,7 @@ const DiaryFilesDashboard = ({ className }) => {
       </section>
       <section>
         <h2>location</h2>
-        <div style={{ border: '1px solid white', height: '300px' }}>
+        <div style={{ border: '1px solid white', height: '400px' }}>
           MAP placeholder
         </div>
       </section>
