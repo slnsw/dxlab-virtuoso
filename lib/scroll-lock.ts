21be56babc8c8879db9158a3bae5e0feb9b46e3c
Luke Dearnley <luked@phm.gov.au>
2020-05-25 12:12:56 +1000

    scroll lock working on diary-files menu

---
+++ lib/scroll-lock.ts
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+
+export const useLockBodyScroll = (isActive = true) => {
+  useEffect(() => {
+    if (process.browser) {
+      // Get original body overflow
+      const originalStyle = window.getComputedStyle(document.body).overflow;
+
+      if (isActive) {
+        console.log('locked');
+        // Prevent scrolling on mount
+        document.body.style.overflow = 'hidden';
+        // Re-enable scrolling when component unmounts
+      } else {
+        console.log('unlocked');
+        document.body.style.overflow = originalStyle;
+      }
+
+      return () => {
+        document.body.style.overflow = originalStyle;
+      };
+    }
+  }, [isActive]); // Empty array ensures effect is only run on mount and unmount
+};
+
+// const $body = document.querySelector('body');
+// let scrollPosition = 0;
+
+// export const scrollLockEnable = () => {
+//   scrollPosition = window.pageYOffset;
+//   $body.style.overflow = 'hidden';
+//   $body.style.position = 'fixed';
+//   $body.style.top = `-${scrollPosition}px`;
+//   $body.style.width = '100%';
+// };
+
+// export const scrollLockDisable = () => {
+//   $body.style.removeProperty('overflow');
+//   $body.style.removeProperty('position');
+//   $body.style.removeProperty('top');
+//   $body.style.removeProperty('width');
+//   window.scrollTo(0, scrollPosition);
+// };
