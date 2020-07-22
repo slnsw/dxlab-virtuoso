ef5ccde114b0a3b0848415f596856d00dc562d88
Kaho Cheung <kaho@elefanttraks.com>
2020-07-22 10:56:15 +1000

    Add reload icon

--- components/Icon/getSVG.tsx
+++ components/Icon/getSVG.tsx
@@ -127,6 +127,22 @@ export function getSVG(name) {
           <path d="M336,336a48.09,48.09,0,0,0-45.25,32H48v32H290.75a48,48,0,0,0,90.5,0H464V368H381.25A48.09,48.09,0,0,0,336,336Z" />
         </g>
       );
+    case 'reload-sharp':
+      return (
+        <g>
+          <path
+            d="M400,148l-21.12-24.57A191.43,191.43,0,0,0,240,64C134,64,48,150,48,256s86,192,192,192A192.09,192.09,0,0,0,421.07,320"
+            style={{
+              fill: 'none',
+              stroke: '#000',
+              strokeLinecap: 'square',
+              strokeMiterlimit: 10,
+              strokeWidth: '32px',
+            }}
+          />
+          <path d="M464,68.45V220a4,4,0,0,1-4,4H308.45a4,4,0,0,1-2.83-6.83L457.17,65.62A4,4,0,0,1,464,68.45Z" />
+        </g>
+      );
 
     default:
       return <path />;
