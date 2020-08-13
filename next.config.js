f8f059c3d8cea4df2d7360328815d531b7e3f838
Kaho Cheung <kaho@elefanttraks.com>
2020-08-13 16:35:47 +1000

    Update next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   // dxlab-virtuoso is proxied to dxlab.sl.nsw.gov.au/virtuoso, so we need to add a prefix for _next JS files
-  assetPrefix: '/virtuoso',
+  ...(process.env.NODE_ENV === 'production'
+    ? {
+        assetPrefix: '/virtuoso',
+      }
+    : {}),
   webpack: (config, { dev }) => {
     const customConfig = {
       ...config,
